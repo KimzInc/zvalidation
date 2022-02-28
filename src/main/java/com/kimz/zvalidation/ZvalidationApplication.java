cfb32bfc4b4b6f793720ce1e11cd2d989ba11c5e
KimzInc <kmtflx@gmail.com>
2022-02-28 14:45:39 +0300

    Initial commit

---
+++ src/main/java/com/kimz/zvalidation/ZvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.kimz.zvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZvalidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimz/zvalidation/ZvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimz.zvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
